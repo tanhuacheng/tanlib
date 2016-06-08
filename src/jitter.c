16ad6651c6d89b150f823dc1b041d89544be0f02
tanhuacheng <tanhc.gz@gmail.com>
2016-06-08 17:34:08 +0800

    jitter 数据长度类型定义为 int32_t

--- src/jitter.c
+++ src/jitter.c
@@ -1,7 +1,7 @@
 /*
 *********************************************************************************************************
 * 文件: jitter.c
-* 版本: V0.01
+* 版本: V0.02
 * 创建: Tue May 31 11:23:49 2016
 * 作者: 谭化成
 * 描述: 抖动缓冲区
@@ -21,21 +21,21 @@
 /* 私有数据类型声明 -----------------------------------------------------------------------------------*/
 struct jitter {
     struct {
-        uint16_t max;
-        uint16_t hig;
-        uint16_t std;
-        uint16_t low;
-        uint16_t met;
-        uint16_t size;
-        uint16_t unit;
+        uint32_t max;
+        uint32_t hig;
+        uint32_t std;
+        uint32_t low;
+        uint32_t met;
+        size_t size;
+        uint32_t unit;
     } param;
     queue_array_t queue;
     enum {
         JITTER_STANDBY,
         JITTER_READY,   
     } state;
-    uint16_t cnt_drop;
-    uint16_t cnt_insert;
+    uint32_t cnt_drop;
+    uint32_t cnt_insert;
     pthread_mutex_t lock;
 };
 
@@ -45,12 +45,15 @@ struct jitter {
 
 /* 函数定义 -------------------------------------------------------------------------------------------*/
 // size: 数据单元字节数, unit: 每次读取的数据单元数
-jitter_t* jitter_create (const size_t size, const uint16_t unit)
+jitter_t* jitter_create (const size_t size, const int32_t unit)
 {
     jitter_t* jitter;
-    void* buffer;
+    void* buff;
 
-    if ((0 == size) || (0 == unit)) {
+    if ((0 == size) || (0 >= unit)) {
+        return NULL;
+    }
+    if (INT32_MAX < (unit * JITTER_PARAM_MUL_MAX)) {
         return NULL;
     }
 
@@ -71,12 +74,12 @@ jitter_t* jitter_create (const size_t size, const uint16_t unit)
         goto err_free_jitter;
     }
 
-    if (NULL == (buffer = malloc(size * jitter->param.max))) {
+    if (NULL == (buff = malloc(size * jitter->param.max))) {
         goto err_destroy_lock;
     }
 
-    if (queue_array_init(&jitter->queue, buffer, size, jitter->param.max) < 0) {
-        goto err_free_buffer;
+    if (queue_array_init(&jitter->queue, buff, size, jitter->param.max) < 0) {
+        goto err_free_buff;
     }
 
     jitter->state = JITTER_STANDBY;
@@ -85,8 +88,8 @@ jitter_t* jitter_create (const size_t size, const uint16_t unit)
 
     return jitter;
 
-err_free_buffer:
-    free(buffer);
+err_free_buff:
+    free(buff);
 err_destroy_lock:
     pthread_mutex_destroy(&jitter->lock);
 err_free_jitter:
@@ -96,16 +99,16 @@ err_free_jitter:
 }
 
 // 读取长度 = unit; 返回: -1 = 出现错误, 0 = 正常读取, >0 = 补零数量.
-int32_t jitter_read (jitter_t* jitter, void* buffer)
+int32_t jitter_read (jitter_t* jitter, void* buff)
 {
     int32_t retn = 0;
 
-    if ((NULL == jitter) || (NULL == buffer)) {
+    if ((NULL == jitter) || (NULL == buff)) {
         return -1;
     }
 
     pthread_mutex_lock(&jitter->lock);
-    memset(buffer, 0, jitter->param.size * jitter->param.unit);
+    memset(buff, 0, jitter->param.size * jitter->param.unit);
 
     if (JITTER_READY == jitter->state) {
         int32_t length = queue_array_length(&jitter->queue);
@@ -136,7 +139,7 @@ int32_t jitter_read (jitter_t* jitter, void* buffer)
 
         int res;
 
-        if ((res = queue_array_read(&jitter->queue, buffer, read)) < 0) {
+        if ((res = queue_array_read(&jitter->queue, buff, read)) < 0) {
             jitter->cnt_insert = 0;
             pthread_mutex_unlock(&jitter->lock);
             return -1;
@@ -156,23 +159,23 @@ int32_t jitter_read (jitter_t* jitter, void* buffer)
 }
 
 // 返回: -1 = 出现错误, 0 = 正常写入, >0 = 丢弃数量.
-int32_t jitter_write (jitter_t* jitter, const void* buffer, uint16_t length)
+int32_t jitter_write (jitter_t* jitter, const void* buff, int32_t length)
 {
-    int32_t buffer_length;
+    int32_t buff_length;
     int32_t retn = 0;
 
-    if ((NULL == jitter) || (NULL == buffer)) {
+    if ((NULL == jitter) || (NULL == buff)) {
         return -1;
     }
 
     pthread_mutex_lock(&jitter->lock);
-    if ((buffer_length = queue_array_length(&jitter->queue)) < 0) {
+    if ((buff_length = queue_array_length(&jitter->queue)) < 0) {
         pthread_mutex_unlock(&jitter->lock);
         return -1;
     }
 
-    if (buffer_length >= jitter->param.hig) {
-        jitter->cnt_drop += buffer_length;
+    if (buff_length >= jitter->param.hig) {
+        jitter->cnt_drop += buff_length;
         if (jitter->cnt_drop >= jitter->param.met) {
             jitter->cnt_drop = 0;
             length -= 1;
@@ -184,13 +187,13 @@ int32_t jitter_write (jitter_t* jitter, const void* buffer, uint16_t length)
 
     int32_t res;
 
-    if ((res = queue_array_write(&jitter->queue, buffer, length)) < 0) {
+    if ((res = queue_array_write(&jitter->queue, buff, length)) < 0) {
         jitter->cnt_drop = 0;
         pthread_mutex_unlock(&jitter->lock);
         return -1;
     }
     retn += length - res;
-    if ((buffer_length + res) >= jitter->param.std) {
+    if ((buff_length + res) >= jitter->param.std) {
         jitter->state = JITTER_READY;
     }
     pthread_mutex_unlock(&jitter->lock);
