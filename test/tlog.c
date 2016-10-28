11eac728b4d11354002c9b39996e75962af2184d
tanhuacheng <tanhc.gz@gmail.com>
2016-10-28 12:22:12 +0800

    tlog.c 测试项目增加 assert 断言

--- test/tlog.c
+++ test/tlog.c
@@ -2,6 +2,7 @@
 // 测试 log(记录日志消息)
 
 /* 包含头文件 ------------------------------------------------------------------------------------*/
+#include <assert.h>
 #include "log.h"
 
 /* 函数定义 --------------------------------------------------------------------------------------*/
@@ -10,16 +11,16 @@ int main(int argc, char *argv[])
     (void)argc;
     (void)argv;
 
-    LOGE("test");
-    LOGE("test: %d\n", 1);
+    assert(LOGE("test") >= 0);
+    assert(LOGE("test: %d\n", 1) >= 0);
 
-    LOGW("test\n");
-    LOGW("test: %d\n", 1);
+    assert(LOGW("test\n") >= 0);
+    assert(LOGW("test: %d\n", 1) >= 0);
 
-    LOGI("test\n");
-    LOGI("test: %d\n", 1);
+    assert(LOGI("test\n") >= 0);
+    assert(LOGI("test: %d\n", 1) >= 0);
 
-    LOG_NEWLINE();
+    assert(LOG_NEWLINE() >= 0);
 
     return 0;
 }
